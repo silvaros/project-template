92b3b09dcb876f7e2c6bc703459ea9acf0d89a05
Brad Kahl <bkahl1984@gmail.com>
2015-04-15 02:16:08 -0400

    validation start, works for email but its hacked

--- public/modules/core/models/notifyMeModel.js
+++ public/modules/core/models/notifyMeModel.js
@@ -2,16 +2,59 @@ define([
 	'backbone'
 ],
 function(Backbone){
-	var NotifyMeModel = Backbone.Model.extend({
+	var NotifyMeModel = Backbone.Model.extend({		
+		url: function(){
+			return "/api/notifyme";	
+		},
+
 		defaults: { 
 			email: '',
 			zipCode: '',
 			age:''
 		},
 
-		url: function(){
-			return "/api/notifyme";	
-		}
+		// currently this is used
+		validate: function (attrs) {
+		    var errors = [];
+
+		    if (!attrs.email) {
+		        errors.push({name: 'email', message: 'Please add an email.'});
+		    }
+		    if (!attrs.zipCode) {
+		        errors.push({name: 'zipcode', message: 'Please add a zipcode.'});
+		    }
+
+		    return errors.length > 0 ? errors : false;
+		},
+
+		// would like to use this way
+		validation: {
+	        email: [{
+		      required: true,
+		      msg: 'Please enter an email address'
+		    },{
+		      pattern: 'email',
+		      msg: 'Please enter a valid email'
+		    }],
+	        // password: {
+	        //     minLength: 8
+	        // },
+	        // repeatPassword: {
+	        //     equalTo: 'password',
+	        //     msg: 'The passwords does not match'
+	        // },
+	        // gender: {
+	        //     required: false
+	        // },
+	       	zipCode: {
+	            required: true,
+	            msg: 'Please enter a valid zipcode'
+	        },
+	        age: {
+	            required: false,
+	            range: [18, 100]
+	        }
+	    }
 	});
 
 	return NotifyMeModel;
--- public/modules/core/viewmodels/notifyMeViewModel.js
+++ public/modules/core/viewmodels/notifyMeViewModel.js
@@ -1,24 +1,56 @@
 define([
 	'knockout',
-	'knockback'
+	'knockback',
+	'jquery'
 ],
-function(ko, kb){
+function(ko, kb, $){
 	return kb.ViewModel.extend({
+
 	    constructor: function(model, options) {
 	    	this.model = ko.observable(model);
 		    this.email = kb.observable(model, {key: 'email'});
 		    this.zipCode = kb.observable(model, {key: 'zipCode'});
 		    this.age = kb.observable(model, {key: 'age'});
+
+		   	this.showErrors = function(errors) {
+			    _.each(errors, function (error) {
+			        var controlGroup = this.$('.' + error.name);
+			        controlGroup.addClass('error');
+			        controlGroup.find('.help-inline').text(error.message);
+			    }, this);
+			};
+
+			this.hideErrors = function () {
+			    this.$('.control-group').removeClass('error');
+			    this.$('.help-inline').text('');
+			};
 	    },
 
 	    onNotifyMeClick: function(vm, e){
+	    	var self = this;
+
+	    	console.log("vm.email : ", vm.email());
+	    	console.log("vm.zipCode : ", vm.zipCode());
+	    	console.log("vm.age : ", vm.age());
+
+	    	if(!vm.email()) {
+	    		$('#email').css('display','block');
+	    	}
+
+	    	if(vm.email()) {
+	    		$('#email').css('display','none');
+	    	}
+
 	    	vm.model().save(null, {
 	    		success: function(){
+	    			//self.hideErrors(errors);
 	    			debugger	
 	    		},
 	    		error: function() {
+	    			//self.showErrors(errors);
 	    			debugger
-	    		}
+	    		},
+	    		validate: true
 	    	})
 	    }
 	});
