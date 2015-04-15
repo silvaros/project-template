647dfcd47bbf8e26255a08c5b7433c7738401ad3
Brad Kahl <bkahl1984@gmail.com>
2015-04-15 04:14:35 -0400

    update merge conflict by commenting out jacobs work until we can discuss

--- public/modules/core/viewmodels/notifyMeViewModel.js
+++ public/modules/core/viewmodels/notifyMeViewModel.js
@@ -12,6 +12,9 @@ function(ko, kb, $){
 		    this.zipCode = kb.observable(model, {key: 'zipCode'});
 		    this.age = kb.observable(model, {key: 'age'});
 
+		    // Temporary for Presentation
+		    this.emailSubmitted = ko.observable("");
+
 		   	this.showErrors = function(errors) {
 			    _.each(errors, function (error) {
 			        var controlGroup = this.$('.' + error.name);
@@ -24,6 +27,20 @@ function(ko, kb, $){
 			    this.$('.control-group').removeClass('error');
 			    this.$('.help-inline').text('');
 			};
+
+			this.signup = function(vm) {
+		    	return vm.model().save(null, {
+		    		success: function(){
+		    			//self.hideErrors(errors);
+		    			//debugger	
+		    		},
+		    		error: function() {
+		    			//self.showErrors(errors);
+		    			//debugger
+		    		},
+		    		validate: true
+		    	});	
+			}
 	    },
 
 	    onNotifyMeClick: function(vm, e){
@@ -33,24 +50,52 @@ function(ko, kb, $){
 	    	console.log("vm.zipCode : ", vm.zipCode());
 	    	console.log("vm.age : ", vm.age());
 
-			var emailRx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    		if( !vm.email() || !emailRx.test(vm.email()) ) {
-	    		$('#email').css('display','block');
-	    	}
-	    	else {
-				$('#email').css('display','none');
+//COMMENTING OUT FOR NOW SINCE I RAN INTO A CONFLICT
+
+			// var emailRx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+   //  		if( !vm.email() || !emailRx.test(vm.email()) ) {
+	  //   		$('#email').css('display','block');
+	  //   	}
+	  //   	else {
+			// 	$('#email').css('display','none');
 		    	
-		    	vm.model().save(null, {
-		    		success: function(model, resp){
-		    			$('#confirmMsg').css('display','block').text(resp.responseJSON.msg);
-		    			//self.hideErrors(errors);
-		    		},
-		    		error: function() {
-		    			//self.showErrors(errors);
-		    		},
-		    		validate: false
-		    	})
-		    }
+		 //    	vm.model().save(null, {
+		 //    		success: function(model, resp){
+		 //    			$('#confirmMsg').css('display','block').text(resp.responseJSON.msg);
+		 //    			//self.hideErrors(errors);
+		 //    		},
+		 //    		error: function() {
+		 //    			//self.showErrors(errors);
+		 //    		},
+		 //    		validate: false
+		 //    	})
+		 //    }
+
+	    	// HACK FOR NOW UNTIL WE CAN GET BACKBONE VALIDATION PROPERLY IMPLEMENTED
+	    	if(!vm.email()) {
+	    		$('.alert.email').css('display','block');
+	    	}
+
+	    	if(!vm.zipCode()) {
+	    		$('.alert.zipcode').css('display','block');
+	    	}
+
+	    	if(vm.email()) {
+	    		$('.alert.email').css('display','none');
+	    	}
+	    	if(vm.zipCode()) {
+	    		$('.alert.zipcode').css('display','none');
+	    	}
+
+	    	if(vm.email() && vm.zipCode()) {
+	    		$('.notify-form.signup').css('display','none');
+	    		$('.notify-form.success').css('display','block');
+
+	    		self.emailSubmitted(vm.email());
+
+	    		self.signup(vm);
+	    	}
+	    	// END HACK
 	    }
 	});
 })
