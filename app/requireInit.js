08279f5e5a5ed4c679d4600041f028259355bfa4
Jacob Knitter <jacob.knitter@live.com>
2015-04-20 04:01:12 -0400

    rename configs
    add tasks to build production

---
+++ app/requireInit.js
@@ -0,0 +1,15 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+	baseUrl: './app/',
+	nodeRequire: require,
+	waitSeconds: 0,
+
+	// please keep in abc order
+	paths: {
+		'codeGen': '../utils/codeGenerator',
+		'globber': '../utils/globber'
+	}
+});
+
+module.exports = requirejs;
