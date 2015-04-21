1ecbb30f792141e42e9ccb0a2cc3190b122ad121
Jacob Knitter <jacob.knitter@live.com>
2015-04-20 22:47:53 -0400

    remove cleanup, delete stuff on rjs callback

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -8,46 +8,60 @@ var exec = require('child_process').exec,
 	//minifyCSS = require('gulp-minify-css'),
     uglify = require('gulp-uglify');
 
-var prodRoot = '../production/public';
+var basePath = '../';
+var prodRoot = basePath+'production/public';
 
-// main task to bundle the app
-gulp.task('prod', ['prod-copy-app-folder', 'prod-rjs', 'prod-less', 'prod-cleanup' ]);
+//**** main tasks to bundle the app ****//
+gulp.task('prod', ['prod-copy', 'prod-rjs', 'prod-less']);
 
-gulp.task('prod-copy-app-folder', function(){
+gulp.task('prod-copy', function(){
 	// copy all under app, keep data folder too, but no data
-	gulp.src(['app/**/*', '!app/data/**/*'], { "base" : "." })
-	.pipe(gulp.dest('production'));
+	gulp.src(['app/**/*'], {cwd: basePath })
+	.pipe(gulp.dest(basePath+'production/app'));
+	console.log("prod-copy-app complete");
+
+	/* copy all package files '.bowerrc, .gitignore, bower.json, package.json, server.js, 
+	keep data folder too, but no data */
+	gulp.src(['.bowerrc', 'bower.json', 'package.json', 'server.js'], {cwd: basePath })
+	.pipe(gulp.dest(basePath+'production'));
+	console.log("prod-copy-package-files complete");
 });
 
 gulp.task('prod-rjs', function(){
 	exec("node r.js -o rjs.build.js", function(err, stdout, stderr) {
    		if(err) console.log(err);
+
+		// delete less folder
+		del(prodRoot + '/less', {force: true});
+		console.log("delete production/less");
+
+		// remove copied lib folder when we npm install the build let bower take over
+		del(prodRoot + '/lib', {force: true});
+		console.log("delete production/lib");   	
    	}); 
+
+   	console.log("rjs complete");
 });
 
 gulp.task('prod-less', function(){
-	gulp.src([ prodRoot + '/less/app.less'])
+	gulp.src(['public/less/app.less'], {cwd: basePath })
 		.pipe(less())
-		.pipe(gulp.dest('production/public/css'));
+		.pipe(gulp.dest(basePath+'production/public/css'));
+	console.log("prod-less complete");
 });
 
-gulp.task('prod-cleanup', function(){
-	// delete less files after compiling css
-	//del(prodRoot + '/less', {force: true});
-	// remove copied lib folder when we npm install the build let bower take over
-	//del(prodRoot + '/lib', {force: true});
-});
 
+//**** end main tasks to bundle the app ****//
 
 
 gulp.task('styles', function() {
-    return gulp.src(['../public/less/app.less'])
+    return gulp.src(['public/less/app.less'], {cwd: basePath })
                 .pipe(less())
-                .pipe(gulp.dest('../public/css'));
+                .pipe(gulp.dest(basePath+'public/css'));
 });
 
 gulp.task('wless', ['styles'], function() {
-    gulp.watch('../public/less/**', ['styles']);
+    gulp.watch(basePath+'public/less/**', ['styles']);
 });
 
 
