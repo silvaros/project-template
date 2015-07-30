0234e88172dc75161b17165a4b20bd3d57770391
silvaros <jacob.knitter@live.com>
2015-07-30 17:32:09 -0400

    all urls lead to index

--- app/routes/coreRoutes.js
+++ app/routes/coreRoutes.js
@@ -1,7 +1,7 @@
 define(function(){
 	return function(app){
 		// serve an empty page that just loads the browserify bundle
-		app.route('/').get(function(req, res) {
+		app.route('/*').get(function(req, res) {
 		    res.render('index');
 		});
 	}
